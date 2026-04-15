abcd838ad405fd241c38eea065773a540c25bbd3
Tasneem Attaallah <t.attaallah@ext.instadeep.com>
2026-04-15 15:53:30 +0200

    Enhance CLIPContrastiveLoss with label smoothing
    Added label smoothing parameter to CLIPContrastiveLoss for improved model calibration and regularization.

--- src/training/loss.py
+++ src/training/loss.py
@@ -5,14 +5,15 @@ import math
 from ..utils.config import INIT_TEMP
 
 class CLIPContrastiveLoss(nn.Module):
-    def __init__(self, init_temp=INIT_TEMP):
+    def __init__(self, init_temp=INIT_TEMP, label_smoothing = 0.1):
         super().__init__()
         self.log_temp = nn.Parameter(torch.tensor(math.log(init_temp)))
-
+        self.label_smoothing = label_smoothing # to prevent the model from becoming overconfident, adds a regularization term and acts as a calibration for the accuracy scores
+        
     def forward(self, z_spec, z_pep):
         temp = self.log_temp.exp()
         logits = (z_spec @ z_pep.T) / temp
         labels = torch.arange(logits.size(0), device=logits.device)
-        loss = (F.cross_entropy(logits, labels) + F.cross_entropy(logits.T, labels)) / 2
-        acc = (logits.argmax(dim=1) == labels).float().mean()
+        loss = (F.cross_entropy(logits, labels, label_smoothing=self.label_smoothing) + F.cross_entropy(logits.T, labels, label_smoothing=self.label_smoothing)) / 2
+        acc = ((logits.argmax(dim=1) == labels).float().mean() + (logits.argmax(dim=0) == labels).float().mean()) / 2 # COMPUTE ACC ROW-WISE (SPEC -> PEP) AND COLUMN-WISE (PEP -> SPEC)
         return loss, acc
